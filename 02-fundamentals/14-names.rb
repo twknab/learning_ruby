8e00a5f1732720c4c4c6f6fe875f3027e1e09ac1
twknab <natureminded@users.noreply.github.com>
2017-10-18 18:21:18 -0700

    fixed names

--- 02-fundamentals/14-names.rb
+++ 02-fundamentals/14-names.rb
@@ -20,23 +20,23 @@
         The name is 'Jennifer Smith'
 =end
 
-a = {first_name: "Tim", last_name: "Knab"}
-b = {first_name: "Julianna", last_name: "Giles"}
-c = {first_name: "Chris", last_name: "Knab"}
-d = {first_name: "Danielle", last_name: "Knab"}
-e = {first_name: "John", last_name: "Knab"}
-f = {first_name: "Nancy", last_name: "Seamands"}
-g = {first_name: "Bobbi", last_name: "Giles"}
-h = {first_name: "Kate", last_name: "Gilman"}
-i = {first_name: "Brian", last_name: "Gilman"}
-j = {first_name: "Liam", last_name: "Gilman"}
-k = {first_name: "Lilly", last_name: "Gilman"}
-l = {first_name: "Tom", last_name: "Giles"}
-m = {first_name: "Pete", last_name: "Rogers"}
-n = {first_name: "Becca", last_name: "Rogers"}
-o = {first_name: "Billy", last_name: "Giles"}
-p = {first_name: "Kailee", last_name: "Giles"}
-q = {first_name: "Bill", last_name: "Giles"}
+a = {first_name: "Tim", last_name: "Banks"}
+b = {first_name: "Julianna", last_name: "Manniford"}
+c = {first_name: "Chris", last_name: "Banks"}
+d = {first_name: "Danielle", last_name: "Banks"}
+e = {first_name: "John", last_name: "Banks"}
+f = {first_name: "Nancy", last_name: "Beeman"}
+g = {first_name: "Bobbi", last_name: "Manniford"}
+h = {first_name: "Kate", last_name: "Filmar"}
+i = {first_name: "Brian", last_name: "Filmar"}
+j = {first_name: "Liam", last_name: "Filmar"}
+k = {first_name: "Lilly", last_name: "Filmar"}
+l = {first_name: "Tom", last_name: "Manniford"}
+m = {first_name: "Pete", last_name: "Franken"}
+n = {first_name: "Becca", last_name: "Franken"}
+o = {first_name: "Billy", last_name: "Manniford"}
+p = {first_name: "Kailee", last_name: "Manniford"}
+q = {first_name: "Bill", last_name: "Manniford"}
 family = [a, b, c, d, e, f, g, h, i , j, k, l, m, n, o, p, q]
 
 def list_family array
